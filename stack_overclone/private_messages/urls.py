48e2acb742a400d037adc65e81f081e3662ebd7b
jbhouse <jbmoorhouse1@gmail.com>
2017-08-11 18:01:55 -0500

    created detail view for messages

--- stack_overclone/private_messages/urls.py
+++ stack_overclone/private_messages/urls.py
@@ -4,11 +4,13 @@ from . import views
 app_name = 'private_messages'
 
 urlpatterns = [
-    # url(r'^$',views.MessageList.as_view(),name='list'),
     url(r'^$',
     views.PrivateMessageList.as_view(),
     name='list'),
     url(r'^new/$',
     views.CreatePrivateMessage.as_view(),
     name='new_message'),
+    url(r'^details/(?P<pk>\d+)/$',
+    views.PrivateMessageDetail.as_view(),
+    name='detail'),
 ]
