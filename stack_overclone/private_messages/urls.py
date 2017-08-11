b2841088332eec1a6bece0f853e3c377469708b1
jbhouse <jbmoorhouse1@gmail.com>
2017-08-11 16:35:03 -0500

    created basic user-user messaging with prefetching in the views to reduce database queries

--- stack_overclone/private_messages/urls.py
+++ stack_overclone/private_messages/urls.py
@@ -4,6 +4,11 @@ from . import views
 app_name = 'private_messages'
 
 urlpatterns = [
-    url(r'^$',views.MessageList.as_view(),name='list'),
-    url(r'^new/$', views.CreateMessage.as_view(),name='new_message'),
+    # url(r'^$',views.MessageList.as_view(),name='list'),
+    url(r'^$',
+    views.PrivateMessageList.as_view(),
+    name='list'),
+    url(r'^new/$',
+    views.CreatePrivateMessage.as_view(),
+    name='new_message'),
 ]
--- stack_overclone/private_messages/views.py
+++ stack_overclone/private_messages/views.py
@@ -2,23 +2,40 @@ from django.shortcuts import render
 from . import forms
 from private_messages.forms import MessageForm
 from django.views import generic
+from . import models
 from private_messages.models import PrivateMessage
 from django.contrib.auth import get_user_model
 User = get_user_model()
 from braces.views import SelectRelatedMixin
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
-class CreateMessage(generic.CreateView,SelectRelatedMixin):
+class CreatePrivateMessage(generic.CreateView,SelectRelatedMixin,LoginRequiredMixin):
     form_class = forms.MessageForm
     success_url = reverse_lazy("home")
     template_name = "private_messages/new_message.html"
 
+    def form_valid(self,form):
+        self.object = form.save(commit=False)
+        self.object.sender_id = self.request.user.id
+        self.object.save()
+        return super().form_valid(form)
 
-class MessageList(generic.ListView):
-    model = PrivateMessage
+class PrivateMessageList(generic.ListView):
+    model = models.PrivateMessage
+    template_name = 'private_messages/privatemessage_list.html'
 
-class MessageDetail(SelectRelatedMixin,generic.DetailView):
+    def get_queryset(self):
+        self.message_recipient = User.objects.prefetch_related('recipient').get(username__iexact=self.request.user.username)
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['users_private_messages'] = self.message_recipient.recipient.all()
+        return context
+
+class PrivateMessageDetail(SelectRelatedMixin,generic.DetailView):
     model = PrivateMessage
