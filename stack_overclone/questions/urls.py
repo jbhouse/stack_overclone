1bc567fbc1bb0c0cfb3a04992fd71b9765cf6c5f
jbhouse <jbmoorhouse1@gmail.com>
2017-08-11 23:43:36 -0500

    added the ability to find a users questions

--- stack_overclone/questions/urls.py
+++ stack_overclone/questions/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^all/$',views.QuestionList.as_view(),name='list'),
     url(r'^view/(?P<pk>\d+)/$',views.QuestionDetail.as_view(),name='detail'),
     url(r'^delete/(?P<pk>\d+)/$',views.DeleteQuestion.as_view(),name='delete'),
+    url(r"by/(?P<username>[-\w]+)/$",views.UserQuestions.as_view(),name="by_user"),
 ]
