b77283373a0115c0f55ae1304f53cf6d788a2cc8
jbhouse <jbmoorhouse1@gmail.com>
2017-08-21 15:32:15 -0500

    reduced visual size of forms

--- stack_overclone/comments/forms.py
+++ stack_overclone/comments/forms.py
@@ -1,20 +1,23 @@
 from django.forms import ModelForm
+from django import forms
 from comments.models import AnswerComment,QuestionComment
 
 class AnswerCommentForm(ModelForm):
+    text = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'comment'}))
     class Meta:
         model = AnswerComment
         fields = ['text']
 
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.fields['text'].label = 'Your Comment'
+    # def __init__(self,*args,**kwargs):
+    #     super().__init__(*args,**kwargs)
+    #     self.fields['text'].label = 'Your Comment'
 
-class QuestionCommentForm(ModelForm):
+class QuestionCommentForm(forms.ModelForm):
+    text = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'comment'}))
     class Meta:
         model = QuestionComment
         fields = ['text']
 
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.fields['text'].label = 'Your Comment'
+    # def __init__(self,*args,**kwargs):
+    #     super().__init__(*args,**kwargs)
+    #     self.fields['text'].label = 'Your Comment'
